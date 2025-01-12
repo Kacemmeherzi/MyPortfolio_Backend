9935d27515df8929ca7393c987a58d6a7fcf5f98
kacem <kacemmeherzi52@gmail.com>
2025-01-12 15:11:50 +0100

    Add AuthController for JWT authentication login endpoint

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private  readonly   string _secretKey;
+public AuthController(IConfiguration configuration){
+    _secretKey = configuration["Jwt:SecretKey"]!;
+}
+
+    [HttpPost("login")]
+    public IActionResult Login(UserLogin userLogin)
+    {
+        if (userLogin.Email == "user" && userLogin.Password == "password")
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, userLogin.Email)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { Token = tokenString });
+        }
+
+        return Unauthorized();
+    }
+}
+
