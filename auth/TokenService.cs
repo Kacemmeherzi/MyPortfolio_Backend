279e288acc8c006b009f8be3ec827bb24b3781d2
kacem <kacemmeherzi52@gmail.com>
2025-01-12 16:01:47 +0100

    Implement token generation in TokenService for JWT authentication

--- auth/TokenService.cs
+++ auth/TokenService.cs
@@ -1,3 +1,7 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 using myportfolio.Models;
 
 public class TokenService : ITokenService
@@ -13,6 +17,18 @@ public class TokenService : ITokenService
 
     public Task<string> GenerateToken(User user)
     {
-        throw new NotImplementedException();
-    }
+          var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:SecretKey"]!);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Email)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            return Task.FromResult(tokenString) ;    }
 }
