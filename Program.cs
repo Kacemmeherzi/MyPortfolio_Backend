a59a8c874b736b7088471ca1c87d70f4889d4666
kacem <kacemmeherzi52@gmail.com>
2025-01-12 15:11:22 +0100

    Add JWT authentication setup in Program.cs Userservice addd to DI scope

--- Program.cs
+++ Program.cs
@@ -1,16 +1,49 @@
+using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// contollers 
 builder.Services.AddControllers();
 
+
+
+// add authentication
+var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:SecretKey"]!);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+//db context
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 
+
+
+// Add services
+builder.Services.AddScoped<IUserService, UserService>();
 var app = builder.Build();
 
 // Map controllers
 app.MapControllers();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
