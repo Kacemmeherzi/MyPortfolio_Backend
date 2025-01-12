462a6b478ac5cf155ad8f145722f2965417ccc59
kacem <kacemmeherzi52@gmail.com>
2025-01-12 16:05:36 +0100

    Remove IUserService dependency from TokenService constructor

--- auth/TokenService.cs
+++ auth/TokenService.cs
@@ -7,12 +7,10 @@ using myportfolio.Models;
 public class TokenService : ITokenService
 {
     private readonly IConfiguration _configuration;
-    private readonly IUserService _userService;
 
-    public TokenService(IConfiguration configuration, IUserService userService)
+    public TokenService(IConfiguration configuration)
     {
         _configuration = configuration;
-        _userService = userService;
     }
 
     public Task<string> GenerateToken(User user)
