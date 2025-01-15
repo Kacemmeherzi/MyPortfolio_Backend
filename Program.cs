2cc4a19ed09110c23b583f154a2b217ce53da826
kacem <kacemmeherzi52@gmail.com>
2025-01-15 16:29:21 +0100

    Enhance JWT authentication handling with custom token validation and error responses

--- Program.cs
+++ Program.cs
@@ -2,13 +2,13 @@ using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-
+using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
+using System.Security.Claims;
 var builder = WebApplication.CreateBuilder(args);
 
 // contollers 
 builder.Services.AddControllers();
-
-
 
 // add authentication
 var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:SecretKey"]!);
@@ -16,10 +16,10 @@ builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
+}).AddJwtBearer(
+    options =>
+{   
+    options.RequireHttpsMetadata = false; 
     options.SaveToken = true;
     options.TokenValidationParameters = new TokenValidationParameters
     {
@@ -28,24 +28,60 @@ builder.Services.AddAuthentication(options =>
         ValidateIssuer = false,
         ValidateAudience = false
     };
+
+    options.Events = new JwtBearerEvents
+    {
+        OnTokenValidated =  async context =>
+        {
+            var tokenService = context.HttpContext.RequestServices.GetRequiredService<TokenService>();
+            ClaimsPrincipal? principal = context.Principal;
+            if (principal != null)
+            {       
+                ClaimsValidation claimsValidation =  await tokenService.ValidateTokenClaims(principal);
+                if (claimsValidation.IsValid!=true)
+                {
+                    context.Fail("Invalid token");
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    context.Response.ContentType = "application/json";
+                }
+            }
+            await Task.CompletedTask;
+        },
+         OnAuthenticationFailed = context =>
+            {
+                // Customize error handling
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "application/json";
+                return context.Response.WriteAsync(JsonSerializer.Serialize(new { message = "Unauthorized" }));
+            },
+    };
 });
 
-//db context
+//db context 
+ 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
-
-
 
 // Add services
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<TokenService>(); // Register TokenService
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication(); // Add authentication middleware
+app.UseAuthorization();
 
 // Map controllers
 app.MapControllers();
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.Run();
