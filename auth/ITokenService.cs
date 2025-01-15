b79aed3b80e3f6cf5946316b1cb89c4710496703
kacem <kacemmeherzi52@gmail.com>
2025-01-15 16:34:48 +0100

    Add ValidateTokenClaims method to ITokenService for claims validation

--- auth/ITokenService.cs
+++ auth/ITokenService.cs
@@ -1,6 +1,11 @@
+using System.Security.Claims;
 using myportfolio.Models;
 
 public interface ITokenService
 {
     Task<string> GenerateToken(User user);
+
+    Task<ClaimsValidation> ValidateTokenClaims(ClaimsPrincipal principal);
+
+    // ? no need to create a token signature method  deja par defaut implementer fel program.cs 
 }
